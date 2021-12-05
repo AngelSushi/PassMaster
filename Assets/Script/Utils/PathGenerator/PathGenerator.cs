b35ba835e633eca32d13c3c189498f0c177eb076
Amaury <amaury.mehlman@gmail.com>
2021-12-05 19:49:21 +0100

    Rotation and Instantiate Plane

--- Assets/Script/Utils/PathGenerator/PathGenerator.cs
+++ Assets/Script/Utils/PathGenerator/PathGenerator.cs
@@ -1,50 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class PathGenerator : MonoBehaviour {
-    public GameObject pathParent;
     public GameObject planePrefab;
     private GameObject target;
 
     public void GeneratePath() {
-        target = pathParent == null ? transform.gameObject : pathParent;
-        bool change = true;
+        target = transform.gameObject;
 
-        int nextIndex = 0;
+        int size = target.transform.childCount;
 
-        for(int i = 0;i</*target.transform.childCount*/ 2;i++) { 
-            if(i > 0) {
-                if(change) 
-                    i--; 
-            }
-
-            if(change) {
-                Debug.Log(i + " " + (i+1));
-                GameObject plane = Instantiate(planePrefab,target.transform.GetChild(i).position,Quaternion.identity);
-                plane.transform.parent = target.transform;
-
-                Vector3 nextPosition = target.transform.GetChild(i + 1).position;
-                float stepDistance = Vector3.Distance(plane.transform.position,nextPosition) / 10f;
-                plane.transform.localScale = new Vector3(0.31f,1f,stepDistance);
-               // COSINUS = adjacent / hypothénus
-                // adjacent = stepDistance
-
-                // Droite perpendiculaire = mx + h
-
-                Vector3 cPoint  = nextPosition + nextPosition.normalized * 15;
-                float cOriginalPosDistance = Vector3.Distance(plane.transform.position,cPoint);
-
-                float angle = Mathf.Cos(stepDistance / cOriginalPosDistance) * Mathf.Rad2Deg;
-
-                Debug.Log("angle: " + angle);
-
-             //   plane.transform.eulerAngles = new Vector3(0f,angle * 100,0f);
-            }
-
-            change = !change;
+        for(int i = 0;i<size;i++) { 
+            GameObject plane = Instantiate(planePrefab,target.transform.GetChild(i).position,Quaternion.identity,target.transform);
+            Vector3 nextPosition = target.transform.GetChild(i + 1).position;
+            float stepDistance = Vector3.Distance(plane.transform.position,nextPosition) / 10f; // Un plane est 10x fois plus grand qu'une unité
+            plane.transform.localScale = new Vector3(0.31f,1f,stepDistance);
+            plane.transform.LookAt(nextPosition);
+            plane.transform.eulerAngles = new Vector3(0f,plane.transform.eulerAngles.y - 178f,0f);
         }
-
-
     }
 }
