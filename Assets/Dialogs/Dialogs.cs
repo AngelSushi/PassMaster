13cbb711e8e6f783cf64c15825f6265f1d0c2d17
Amaury <amaury.mehlman@gmail.com>
2021-12-10 11:24:49 +0100

    Setup Dialogs Window & Object

---
+++ Assets/Dialogs/Dialogs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SimpleDialog", menuName = "Dialogs/SimpleDialog", order = 1)]
+public class Dialogs : ScriptableObject {
+
+    public string name;
+    public string author;
+    public string[] contents;
+    public bool needAnswer;
+    public string[] answers;
+    public bool repeatable;
+
+    // Ajouter des actions pour déclencher le dialogs, des actions après le dialogue
+}
---
+++ Assets/Script/Utils/Dialogs/DialogWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DialogWindow : EditorWindow {
+
+    [MenuItem("Window/Dialogs")]
+    public static void DisplayWindow() {
+        GetWindow<DialogWindow>("Dialogs");
+    }
+
+    void OnGUI() {
+        // Parts Draw 
+        EditorGUI.DrawRect(new Rect(position.width / 3 - 200,0,1,position.height / 2  - 50),Color.gray);
+        EditorGUI.DrawRect(new Rect(position.width / 3 * 2 + 140,0,1,position.height / 2  - 50),Color.gray);
+        EditorGUI.DrawRect(new Rect(0,position.height / 2 - 50,position.width,1),Color.gray);
+
+        // Title Draw
+        GUIStyle labelStyle = GUI.skin.GetStyle("Label");
+       // int size = labelStyle.fontSize;
+      //  labelStyle.fontSize = 15;
+        EditorGUI.LabelField(new Rect((position.width / 3 - 200) / 2 - 50,(position.height / 2  - 50 ) / 2 - 30,100,20),"Any dialogs find",labelStyle);
+       // labelStyle.fontSize = size;
+
+        string[] assetNames = AssetDatabase.FindAssets("Your_Filter", new[] { "Assets/YourFolder" });
+            
+    }
+
+}
